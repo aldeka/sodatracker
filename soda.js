f857a4ec68f33d0f66f46dc7c3c278d977837d05
Karen Rustad <karen.rustad@gmail.com>
2014-04-02 09:35:17 -0700

    added removal button, code cleanup

--- soda.js
+++ soda.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
     var i, j, k;
-    var filters = [];
 
-    /* data */
+    /* 
+     *** Initial app data and state ***
+     */
+
+    var filters = [];
     var sodas = [
         {
             id: 1,
@@ -62,6 +65,38 @@ $(document).ready(function() {
         }
     ];
 
+    /* Fills in all of the templates needed for a given station */
+    var initializeStation = function(station) {
+        var stationID = station.id.toString();
+        // make base station template
+        $('#station-template').tmpl(station).appendTo('.station-list');
+        // fill in sodas the station has
+        $('#the-sodas-template').tmpl(
+            {'sodas': station.sodas}
+            ).appendTo('#station-' + stationID + ' .the-sodas:first-child');
+        // fill in dropdown menus
+        refreshDropdowns(station);
+    };
+
+    /* (Re)instantiates the add and remove dropdown menus for a given station */
+    var refreshDropdowns = function(station) {
+        var stationID = station.id.toString();
+        var remainingSodas = processedSodas(station);
+        $('#station-'+ stationID +' .dropdown-menu').empty();
+        /* Fill in adder menu */
+        $('#soda-dropdown-template').tmpl(
+            {
+                'sodas': remainingSodas
+            }
+            ).appendTo('#station-'+ stationID +' .adder-group .dropdown-menu');
+        /* Fill in remover menu */
+        $('#soda-dropdown-template').tmpl(
+            {
+                'sodas': station.sodas
+            }
+            ).appendTo('#station-'+ stationID +' .remover-group .dropdown-menu');
+    };
+
     /* For a given station, returns list of sodas the station doesn't already have */
     var processedSodas = function(station) {
         var processed = [];
@@ -79,92 +114,8 @@ $(document).ready(function() {
         return processed;
     };
 
-    var initializeStation = function(station) {
-        var stationID = station.id.toString();
-        // make base station template
-        $('#station-template').tmpl(station).appendTo('.station-list');
-        // fill in sodas the station has
-        $('#the-sodas-template').tmpl(
-            {'sodas': station.sodas}
-            ).appendTo('#station-' + stationID + ' .the-sodas:first-child');
-        // fill in dropdown menu with the sodas the station doesn't have
-        $('#soda-dropdown-template').tmpl(
-            {
-                'sodas': processedSodas(station)
-            }
-            ).appendTo('#station-'+ stationID +' .dropdown-menu');
-    };
-
-    var initApp = function() {
-        // initialize soda list
-        for (i=0; i<sodas.length; i++) {
-            $('#soda-template').tmpl(sodas[i]).appendTo('.soda-list');
-        }
-
-        // initialize stations list
-        for (i=0; i<stations.length; i++) {
-            initializeStation(stations[i]);
-        }
-    }
-
-    /* Handler for menu for adding new sodas to stations */
-    $('body').on('click', '.station .dropdown-menu a', function(e) {
-        e.preventDefault();
-        var soda, station, remainingSodas;
-        var sodaID = $(this).attr('data-attribute').slice(5);
-        var stationID = $($(this).parents('.station')[0]).attr('id').slice(8);
-        for (i=0; i<sodas.length; i++){
-            if (sodas[i].id === parseInt(sodaID,10)){
-                soda = sodas[i];
-                break;
-            }
-        }
-        if (soda){
-            for (i=0; i<stations.length; i++) {
-                if (stations[i].id === parseInt(stationID,10)) {
-                    station = stations[i];
-                    // add to station's sodas
-                    station.sodas.push(soda);
-                    $('#station-' + stationID + ' .the-sodas').empty();
-                    // refresh station's list of sodas
-                    $('#the-sodas-template').tmpl(
-                        {'sodas': station.sodas}
-                        ).appendTo('#station-' + stationID + ' .the-sodas');
-                    // refresh dropdown menu
-                    remainingSodas = processedSodas(station);
-                    $('#station-'+ stationID +' .dropdown-menu').empty();
-                    $('#soda-dropdown-template').tmpl(
-                        {
-                            'sodas': remainingSodas
-                        }
-                        ).appendTo('#station-'+ stationID +' .dropdown-menu');
-                    break;
-                }
-            }
-        }
-    });
-
-    /* Filtering stations by soda offered */
-    $('body').on('click', '#sodas ul div', function(e) {
-        var sodaID, acceptable_stations;
-        // toggle soda style
-        $(this).toggleClass('active');
-        sodaID = $(this).attr('data-attribute').slice(5);
-        // toggling filter
-        // check if filter already exists, and if so, remove it
-        for (i=0; i<filters.length; i++) {
-            if (filters[i] === parseInt(sodaID,10)) {
-                filters.splice(i, 1);
-                sodaID = '';
-                break;
-            }
-        }
-        // add new filter, if it exists
-        if (sodaID) {
-            filters.push(parseInt(sodaID,10));
-        }
-        // remove class filtered from all matching stations
-        // add class filtered to any station we want filtered out
+    /* Reapplies classes to the list of stations based on the current filters list */
+    var refilterStations = function() {
         if (filters.length === 0) {
             $('.station').removeClass('filtered');
         } else {
@@ -187,58 +138,9 @@ $(document).ready(function() {
                 $('#station-' + acceptable_stations[i].toString()).removeClass('filtered');
             }
         }
-    });
+    };
 
-    /* Creating a new soda */
-    $('body').on('submit', '#sodamaker', function(e) {
-        e.preventDefault();
-        var newSoda;
-        var sodaName = $(this).find('[name="name"]').val();
-        if (sodaName){
-            newSoda = {
-                id: sodas.length + 1,
-                name: sodaName,
-                color: generateColor()
-            };
-            sodas.push(newSoda);
-            $('#soda-template').tmpl(newSoda).appendTo('.soda-list');
-            for (i=0; i < stations.length; i++) {
-                var id = stations[i].id.toString();
-                var remainingSodas = processedSodas(stations[i]);
-                $('#station-'+ id +' .dropdown-menu').empty();
-                $('#soda-dropdown-template').tmpl(
-                    {
-                        'sodas': remainingSodas
-                    }
-                    ).appendTo('#station-'+ id +' .dropdown-menu');
-            }
-            $(this).find('[name="name"]').val('');
-        }
-    });
-
-    /* Creating a new station */
-    $('body').on('submit', '#stationmaker', function(e) {
-        e.preventDefault();
-        var stationName, stationLoc;
-        stationName = $(this).find('[name="name"]').val();
-        stationLoc = $(this).find('[name="loc"]').val();
-        if (stationName){
-            newStation = {
-                id: stations.length + 1,
-                name: stationName,
-                loc: stationLoc,
-                sodas: []
-            };
-            stations.push(newStation);
-            initializeStation(newStation);
-            $(this).find('[name="name"]').val('');
-            $(this).find('[name="loc"]').val('');
-        }
-    });
-
-    initApp();
-
-    /* Generates random hex color within some brightness constraints*/
+    /* Generates random hex color within some brightness constraints */
     var generateColor = function() {
         var color1, color2, color3, result;
         var minBrightness = 300;
@@ -267,28 +169,178 @@ $(document).ready(function() {
         return "#" + result;
     };
 
-    /* Helper functions */
+    /* Initial app setup */
+    var initApp = function() {
+        // initialize soda list
+        for (i=0; i<sodas.length; i++) {
+            $('#soda-template').tmpl(sodas[i]).appendTo('.soda-list');
+        }
+
+        // initialize stations list
+        for (i=0; i<stations.length; i++) {
+            initializeStation(stations[i]);
+        }
+
+        /* 
+         *** DOM Handlers! ***
+         */
+
+        /* Handler for menu for adding new sodas to stations */
+        $('body').on('click', '.station .adder-group .dropdown-menu a', function(e) {
+            e.preventDefault();
+            var soda, station, remainingSodas;
+            var sodaID = $(this).attr('data-attribute').slice(5);
+            var stationID = $($(this).parents('.station')[0]).attr('id').slice(8);
+            for (i=0; i<sodas.length; i++){
+                if (sodas[i].id === parseInt(sodaID,10)){
+                    soda = sodas[i];
+                    break;
+                }
+            }
+            if (soda){
+                for (i=0; i<stations.length; i++) {
+                    if (stations[i].id === parseInt(stationID,10)) {
+                        station = stations[i];
+                        // add to station's sodas
+                        station.sodas.push(soda);
+                        $('#station-' + stationID + ' .the-sodas').empty();
+                        // refresh station's list of sodas
+                        $('#the-sodas-template').tmpl(
+                            {'sodas': station.sodas}
+                            ).appendTo('#station-' + stationID + ' .the-sodas');
+                        // refresh dropdown menu
+                        refreshDropdowns(station);
+                        break;
+                    }
+                }
+                refilterStations();
+            }
+        });
+
+        /* Handler for menu for removing sodas from stations */
+        $('body').on('click', '.station .remover-group .dropdown-menu a', function(e) {
+            e.preventDefault();
+            var soda, station, remainingSodas;
+            var sodaID = $(this).attr('data-attribute').slice(5);
+            var stationID = $($(this).parents('.station')[0]).attr('id').slice(8);
+            for (i=0; i<sodas.length; i++){
+                if (sodas[i].id === parseInt(sodaID,10)){
+                    soda = sodas[i];
+                    break;
+                }
+            }
+            if (soda){
+                for (i=0; i<stations.length; i++) {
+                    if (stations[i].id === parseInt(stationID,10)) {
+                        station = stations[i];
+                        // remove from station's sodas
+                        station.sodas.splice(station.sodas.indexOf(soda), 1);
+                        $('#station-' + stationID + ' .the-sodas').empty();
+                        // refresh station's list of sodas
+                        $('#the-sodas-template').tmpl(
+                            {'sodas': station.sodas}
+                            ).appendTo('#station-' + stationID + ' .the-sodas');
+                        // refresh dropdown menus
+                        refreshDropdowns(station);
+                        break;
+                    }
+                }
+                refilterStations();
+            }
+        });
+
+        /* Handler for filtering stations by soda offered */
+        $('body').on('click', '#sodas ul div', function(e) {
+            var sodaID, acceptable_stations;
+            // toggle soda style
+            $(this).toggleClass('active');
+            sodaID = $(this).attr('data-attribute').slice(5);
+            // toggling filter
+            // check if filter already exists, and if so, remove it
+            for (i=0; i<filters.length; i++) {
+                if (filters[i] === parseInt(sodaID,10)) {
+                    filters.splice(i, 1);
+                    sodaID = '';
+                    break;
+                }
+            }
+            // add new filter, if it exists
+            if (sodaID) {
+                filters.push(parseInt(sodaID,10));
+            }
+            // remove class filtered from all matching stations
+            // add class filtered to any station we want filtered out
+            refilterStations();
+        });
+
+        /* Handler for creating a new soda */
+        $('body').on('submit', '#sodamaker', function(e) {
+            e.preventDefault();
+            var newSoda;
+            var sodaName = $(this).find('[name="name"]').val();
+            if (sodaName){
+                newSoda = {
+                    id: sodas.length + 1,
+                    name: sodaName,
+                    color: generateColor()
+                };
+                sodas.push(newSoda);
+                $('#soda-template').tmpl(newSoda).appendTo('.soda-list');
+                for (i=0; i < stations.length; i++) {
+                    var id = stations[i].id.toString();
+                    var remainingSodas = processedSodas(stations[i]);
+                    $('#station-'+ id +' .dropdown-menu').empty();
+                    $('#soda-dropdown-template').tmpl(
+                        {
+                            'sodas': remainingSodas
+                        }
+                        ).appendTo('#station-'+ id +' .dropdown-menu');
+                }
+                $(this).find('[name="name"]').val('');
+            }
+        });
+
+        /* Handler for creating a new station */
+        $('body').on('submit', '#stationmaker', function(e) {
+            e.preventDefault();
+            var stationName, stationLoc;
+            stationName = $(this).find('[name="name"]').val();
+            stationLoc = $(this).find('[name="loc"]').val();
+            if (stationName){
+                newStation = {
+                    id: stations.length + 1,
+                    name: stationName,
+                    loc: stationLoc,
+                    sodas: []
+                };
+                stations.push(newStation);
+                initializeStation(newStation);
+                $(this).find('[name="name"]').val('');
+                $(this).find('[name="loc"]').val('');
+            }
+        });
+    };
+
+    initApp();
+
+    /* Helper functions for color generator */
 
     var getRandomInt = function(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     };
 
-    var shuffle = function(array) {
-      var currentIndex = array.length, temporaryValue, randomIndex;
+    var shuffle = function(l) {
+      var currentIndex = l.length;
+      var randomIndex, temp;
 
-      // While there remain elements to shuffle...
       while (0 !== currentIndex) {
-
-        // Pick a remaining element...
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
-
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+        temp = l[currentIndex];
+        l[currentIndex] = l[randomIndex];
+        l[randomIndex] = temp;
       }
 
-      return array;
+      return l;
     };
 });
